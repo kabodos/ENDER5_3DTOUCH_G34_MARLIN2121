1a95990b48efdf02277af4ccc34f7eab4cf6450a
Joaquin Alfonso Rosales Rodriguez <joaquin.rosales@soha.com.mx>
2023-10-16 22:24:28 -0700

    Cambio  Z steps de 400 a 800

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1168,7 +1168,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 415 }  //tt tenia #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 415 }  //tt tenia #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 93 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
